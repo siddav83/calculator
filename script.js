cf9e9e99bd02e30d17794877013f3a1c806ead1b
David Siddle <siddav83@gmail.com>
2021-05-02 01:36:40 +0100

    created a calcultor

---
+++ script.js
@@ -0,0 +1,25 @@
+const one = document.getElementById('one')
+const two = document.getElementById('two')
+const three = document.getElementById('three')
+const four = document.getElementById('four')
+const five = document.getElementById('fi ve')
+const six = document.getElementById('six')
+const seven = document.getElementById('seven')
+const seight = document.getElementById('eight')
+const nine = document.getElementById('nine')
+const zero = document.getElementById('zero')
+const plus = document.getElementById('plus')
+const minus = document.getElementById('minus')
+const divide = document.getElementById('divide')
+const multiple = document.getElementById('multiple')
+const enter = document.getElementById('enter')
+const display = document.getElementById('display')
+const number = document.getElementById('number')
+
+number.addEventListener('click',(e)=> {
+  // const re = preventDefault()
+const numberEntered = parseInt(e.target.innerHTML)
+display.innerHTML = numberEntered
+})
+
+
