4b8acec04b27d8988ac880bca6aec8434a14d990
David Siddle <siddav83@gmail.com>
2021-05-04 09:23:47 +0100

    refactoring js

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
-const one = document.getElementById('one')
-const two = document.getElementById('two')
-const three = document.getElementById('three')
-const four = document.getElementById('four')
-const five = document.getElementById('fi ve')
-const six = document.getElementById('six')
-const seven = document.getElementById('seven')
-const seight = document.getElementById('eight')
-const nine = document.getElementById('nine')
-const zero = document.getElementById('zero')
-const plus = document.getElementById('plus')
-const minus = document.getElementById('minus')
-const divide = document.getElementById('divide')
-const multiple = document.getElementById('multiple')
-const enter = document.getElementById('enter')
-const display = document.getElementById('display')
-const number = document.getElementById('number')
-const lcd = document.getElementById('lcd')
+
+const calculator = document.getElementById('container')
+const keys = document,getElementById('keys')
 
 
-digits.addEventListener('click',(e)=> {
-  // const re = preventDefault()
-const numberEntered = parseInt(e.target.innerHTML)
-lcd.innerHTML = numberEntered
+// function clear() {
+//   let bodmas = ""
+//   let numbers = ""
+//   lcd.innerHTML = ""
+// }
+
+function calculate(strings) {
+ console.log(strings)
+}
+
+
+digits.addEventListener('click',(e) => {
+
 })
 
 
